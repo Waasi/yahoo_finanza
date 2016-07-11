9a4d226f7d0be854462819f8a47c31ecff8e097d
Eric Santos <eric.santos@cometa.works>
2016-07-11 05:52:35 -0400

    [Refactor | Quote and Quote Fetcher] This includes:
    - Refactored QuoteFetcher to speed up fetch with tests
    - Refactored Quote to speed up fetch with tests
    - Refactored Documentation

--- lib/yahoo_finanza/quote.ex
+++ lib/yahoo_finanza/quote.ex
@@ -6,8 +6,7 @@ defmodule YahooFinanza.Quote do
   for a single symbol or multiple symbols.
 
   ## Methods
-      fetch_quote/1
-      fetch_quotes/1
+      fetch/1
   """
 
   use GenServer
@@ -18,35 +17,18 @@ defmodule YahooFinanza.Quote do
   ###
 
   @doc """
-  The fetch_quote/1 method accepts a string containing
-  a symbol and returns a quote as a Map or an error if
-  a invalid symbol was given.
-
-  ## Examples
-      iex> YahooFinanza.Quote.fetch_quote "AAPL"\n
-      {:ok, %{"Symbol" => "AAPL", "Ask" => "119.5", ...}}
-
-      iex> YahooFinanza.Quote.fetch_quote "i don't exist"\n
-      {:error, "invalid symbol"}
-  """
-
-  def fetch_quote(symbol) do
-    GenServer.call(__MODULE__, {:quote, symbol})
-  end
-
-  @doc """
-  The fetch_quotes/1 method accepts a list of strings
+  The fetch/1 method accepts a list of strings
   containing a symbol and returns the quotes as a list
   of Maps. If an invalid symbol is in the list, it will
   be ignored.
 
   ## Example
-      iex> YahooFinanza.Quote.fetch_quote ["AAPL", "FB", "I don't exist"]\n
+      iex> YahooFinanza.Quote.fetch ["AAPL", "FB", "I don't exist"]\n
       {:ok, [%{"Symbol" => "AAPL", "Ask" => "119.5", ...}, %{"Symbol" => "FB", "Ask" => "130.5", ...} }
 
   """
 
-  def fetch_quotes(symbols) do
+  def fetch(symbols) do
     GenServer.call(__MODULE__, {:quotes, symbols})
   end
 
@@ -58,33 +40,26 @@ defmodule YahooFinanza.Quote do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def handle_call({:quote, symbol}, _from, state) do
-    {:reply, quote_for(symbol), state}
-  end
-
   def handle_call({:quotes, symbols}, _from, state) do
-    {:reply, quotes_for(symbols), state}
+    {:reply, {:ok, quotes_for(symbols)}, state}
   end
 
   ###
   # Helpers
   ###
 
-  defp quote_for(symbol) do
-    QuoteFetcher.get(symbol)
-  end
-
   defp quotes_for(symbols) do
-    quotes = (symbols |> Enum.map(&Task.async(fn -> handle_quote(&1) end)) |>
-    Enum.map(&Task.await/1)) |> List.delete(nil)
-
-    {:ok, quotes}
+    symbols |> workloads |>
+    Enum.map(&Task.async(fn -> QuoteFetcher.get(&1) end)) |>
+    Enum.map(&Task.await/1) |> strip
   end
 
-  defp handle_quote(symbol) do
-    case QuoteFetcher.get(symbol) do
-      {:ok, qte} -> qte
-      {:error, "invalid symbol"} -> nil
-    end
+  defp strip(quotes) do
+    quotes |> List.flatten |>
+    Enum.filter(fn item -> item["Ask"] != nil end)
+  end
+
+  defp workloads(symbols) do
+    symbols |> Enum.chunk(50, 50, [])
   end
 end
